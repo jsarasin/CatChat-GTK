42468df46e13023ef231990d3dd6ae94a84684f6
James <james.sarasin@gmail.com>
2016-04-18 22:47:18 -0600

    Organizing

--- cc_http.py
+++ cc_http.py
@@ -6,71 +6,71 @@ import hashlib
 
 
 class cc_http(object):
-    server_url = 'http://azenguard.com/CatChat/'
-    username='james'
-    password='computer'
-    userid=0
+	server_url = 'http://azenguard.com/CatChat/'
+	username='james'
+	password='computer'
+	userid=1
 
-    def md5(self, fname):
-        hash_md5 = hashlib.md5()
-        with open(fname, "rb") as f:
-            for chunk in iter(lambda: f.read(4096), b""):
-                hash_md5.update(chunk)
-        return hash_md5.hexdigest()
+	def md5(self, fname):
+		hash_md5 = hashlib.md5()
+		with open(fname, "rb") as f:
+			for chunk in iter(lambda: f.read(4096), b""):
+				hash_md5.update(chunk)
+		return hash_md5.hexdigest()
 
-    def get_chat_from_id_range(self, chat_id, after=0, before=""):
-        r = requests.post(self.server_url + 'getchat.php', data={               'chatroom': str(chat_id),
-                                                                                'rangetype': "id",
-                                                                                'after': str(after),
-                                                                                'before': str(before)
-                                                                                } )
-        return json.loads(r.text)
+	def get_chat_from_id_range(self, chat_id, after=0, before=""):
+		r = requests.post(self.server_url + 'getchat.php', data={			   'chatroom': str(chat_id),
+																				'rangetype': "id",
+																				'after': str(after),
+																				'before': str(before)
+																				} )
+		return json.loads(r.text)
 
-    def say(self, chat_id, message):
-        now = datetime.datetime.now()
-        r = requests.post(self.server_url + 'writechat.php', data={             'chatroom': str(chat_id),
-                                                                                'message':message,
-                                                                                'msg_written': str(now),
-                                                                                'message_type' : 0,
-                                                                                'creator_id' : self.userid
-                                                                                })
-    def say_picture(self, chat_id, hash):
-        now = datetime.datetime.now()
-        r = requests.post(self.server_url + 'writechat.php', data={             'chatroom': str(chat_id),
-                                                                                'message':hash,
-                                                                                'msg_written': str(now),
-                                                                                'message_type' : 1,
-                                                                                'creator_id' : self.userid
-                                                                                })
+	def say(self, chat_id, message):
+		now = datetime.datetime.now()
+		r = requests.post(self.server_url + 'writechat.php', data={			 'chatroom': str(chat_id),
+																				'message':message,
+																				'msg_written': str(now),
+																				'message_type' : 0,
+																				'creator_id' : self.userid
+																				})
+	def say_picture(self, chat_id, hash):
+		now = datetime.datetime.now()
+		r = requests.post(self.server_url + 'writechat.php', data={			 'chatroom': str(chat_id),
+																				'message':hash,
+																				'msg_written': str(now),
+																				'message_type' : 1,
+																				'creator_id' : self.userid
+																				})
 
-    def send_file(self, chat_id, filename):
-        md5_file = self.md5(filename)
-        now = datetime.datetime.now()
-        files = {'userfile':open(filename, 'rb')}
-        datas = {                                                               'creator_id':self.userid,
-                                                                                'msg_written': str(now),
-                                                                                'chatroom': str(chat_id),
-                                                                                }
-        r = requests.post(self.server_url + 'writechatfile.php', data=datas, files=files)
+	def send_file(self, chat_id, filename):
+		md5_file = self.md5(filename)
+		now = datetime.datetime.now()
+		files = {'userfile':open(filename, 'rb')}
+		datas = {															   'creator_id':self.userid,
+																				'msg_written': str(now),
+																				'chatroom': str(chat_id),
+																				}
+		r = requests.post(self.server_url + 'writechatfile.php', data=datas, files=files)
 
-    def check_hash_exists(self, hash):
-        datas = {                                                               'hash':hash
-                                                                                }
+	def check_hash_exists(self, hash):
+		datas = {															   'hash':hash
+																				}
 
-        r = requests.post(self.server_url + "check_hash_exists.php", data=datas)
-        reply = json.loads(r.text)
+		r = requests.post(self.server_url + "check_hash_exists.php", data=datas)
+		reply = json.loads(r.text)
 
-        return reply['exists']
+		return reply['exists']
 
-    def get_picture_thumbnail(self, hash):
-        r = requests.post(self.server_url + "thumbnails/" + hash)
+	def get_picture_thumbnail(self, hash):
+		r = requests.post(self.server_url + "thumbnails/" + hash)
 
-        if(r.headers['content-type'] == "image/png"):
-            open("thumbnails/" + hash, 'wb').write(r.content)
-            return True
-        else:
-            print "get_picture_thumbnail failed!, Status Code: " + str(r.status_code) #+ str(r.headers.items()) + "\n"
-            return r.status_code
+		if(r.headers['content-type'] == "image/png"):
+			open("thumbnails/" + hash, 'wb').write(r.content)
+			return True
+		else:
+			print "get_picture_thumbnail failed!, Status Code: " + str(r.status_code) #+ str(r.headers.items()) + "\n"
+			return r.status_code
 # text/plain
 # image/png
 
--- cc_pictures.py
+++ cc_pictures.py
@@ -5,19 +5,19 @@ from gi.repository import Gtk, Gdk, Pango, GdkPixbuf
 import cc_http, os
 
 def download_thumbnail_from_hash(cc_http_connection, hash):
-    return cc_http_connection.get_picture_thumbnail(hash)
+	return cc_http_connection.get_picture_thumbnail(hash)
 
 def get_pixbuf_thumbnail_from_hash(cc_http_connection, hash):
-    # Do we already have the file?
-    cached_file_name = "thumbnails/" + hash
+	# Do we already have the file?
+	cached_file_name = "thumbnails/" + hash
 
-    if os.path.isfile(cached_file_name) == False:
-        if download_thumbnail_from_hash(cc_http_connection, hash):
-            picture = GdkPixbuf.Pixbuf.new_from_file(cached_file_name)
-        else:
-            picture = GdkPixbuf.Pixbuf.new_from_file("missing.png")
-    else:
-        picture = GdkPixbuf.Pixbuf.new_from_file(cached_file_name)
+	if os.path.isfile(cached_file_name) == False:
+		if download_thumbnail_from_hash(cc_http_connection, hash):
+			picture = GdkPixbuf.Pixbuf.new_from_file(cached_file_name)
+		else:
+			picture = GdkPixbuf.Pixbuf.new_from_file("missing.png")
+	else:
+		picture = GdkPixbuf.Pixbuf.new_from_file(cached_file_name)
 
 
-    return picture
+	return picture
